c107deb4da314a2e38d078da061b610e83b4dc5b
euberdeveloper <euberdeveloper@gmail.com>
2022-07-08 01:12:55 +0200

    ,
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const riddle = 'Riddle???';
+const riddle = 'Riddle';
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/json'});
