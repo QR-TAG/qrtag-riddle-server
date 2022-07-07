eb16f976ae9291cf9b7a86dc1710b70d9f2c64be
euberdeveloper <euberdeveloper@gmail.com>
2022-07-08 00:58:39 +0200

    FIX
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ const riddle = 'What has a head, a tail, is brown, and has no legs?';
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/json'});
   res.end(riddle);
-}).listen(process.env.port ?? 3000);
+}).listen(process.env.PORT ?? 3000);
