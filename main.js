a58d310f1875685915732eee784ea61b7667f7e6
euberdeveloper <euberdeveloper@gmail.com>
2022-07-21 19:02:18 +0200

    feat: update
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
-const http = require('http');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const riddle = 'Riddle';
+const dotenv = require('dotenv');
+dotenv.config();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/json'});
-  res.end(riddle);
-}).listen(process.env.PORT ?? 3000);
+const PORT = process.env.PORT ?? 3000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(MONGODB_URL, { useNewUrlParser: true });
+const db = client.db('qrtag');
+const collection = db.collection('riddles');
+
+app.get('/riddle', async (_req, res) => {
+  const riddle = await collection.findOne({ id: 0 });
+  res.send(riddle);
+});
+
+app.post('/riddle', async (req, res) => {
+  const riddle = {...req.body, id: 0};
+  await collection.updateOne({ id: 0 }, { $set: riddle });
+  res.send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
